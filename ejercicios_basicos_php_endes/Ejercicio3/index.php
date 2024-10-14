4b7e1cec3914cf4134d10cf6f78fa18f26dc367d
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2024-10-14 13:22:04 +0200

    Delete ejercicios_basicos_php_endes/Ejercicio3/index.php

--- ejercicios_basicos_php_endes/Ejercicio3/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$a = 10;
-$b = 20;
-echo "Suma: " . ($a + $b) . "<br>";
-echo "Resta: " . ($a - $b) . "<br>";
-echo "Multiplicación: " . ($a * $b) . "<br>";
-echo "División: " . ($a / $b);
-?>
